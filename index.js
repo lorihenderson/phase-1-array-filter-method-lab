fddb0f42cc37d7460ce1849c480b746d235c4b15
Lori Henderson <lorim95@gmail.com>
2022-05-22 20:48:55 -0400

    commit tree 683ab5f7799adc95b58065535a5d39438a2a79e5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, names) {
+    const x = drivers.filter(function(names2) {
+        return names===names2 || names.toLowerCase() === names2
+    })
+    return x;
+}
+
+function fuzzyMatch(drivers, letter) {
+    const w = drivers.filter(function(anotherLetter) {
+        return anotherLetter.startsWith(letter)
+    })
+    return w;
+}
+
+function matchName(drivers, str) {
+    const pikachu = drivers.filter(function(squirtle) {
+        // console.log(drivers.name)
+        return squirtle.name === str
+    })
+    return pikachu;
+}
